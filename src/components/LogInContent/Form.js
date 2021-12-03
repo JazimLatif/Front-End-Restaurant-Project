e5d17685d52ec541c28a48a8eb31420895b7ab52
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-12-03 00:40:11 +0000

    Add placeholders to login fields, change when email/password is wrong

--- src/components/LogInContent/Form.js
+++ src/components/LogInContent/Form.js
@@ -5,7 +5,12 @@ const Form = ({ onLogIn }) => {
 
     const history = useHistory();
     const [email, setEmail] = useState("");
+    const [incorrectEmail, setIncorrectEmail] = useState(null)
     const [password, setPassword] = useState("");
+    const [incorrectPassword, setIncorrectPassword] = useState(null)
+
+    const emailPlaceholder = !incorrectEmail ? "Enter email..." : "Email not found!"
+    const passwordPlaceholder = !incorrectPassword ? "Enter password..." : "Incorrect password!"
 
     const handleEmailChange = (event) => {
         setEmail(event.target.value);
@@ -22,13 +27,17 @@ const Form = ({ onLogIn }) => {
         await fetch("http://localhost:8080/customer")
         .then(response => response.json())
         .then(data => users = data)
-        
-        const userFound = users.find(user => user.email === email && user.number === parseInt(password))
-        if (userFound) {
-            onLogIn(userFound);
-            history.push("/");
+
+        const userFound = users.find(user => user.email === email);
+        if (!userFound) {
+            setEmail("")
+            setIncorrectEmail(true)
+        } else if (userFound.number !== parseInt(password)) {
+            setPassword("")
+            setIncorrectPassword(true)
         } else {
-            setEmail("");
+            onLogIn(userFound);
+            history.push("/")
         }
     }
 
@@ -37,11 +46,13 @@ const Form = ({ onLogIn }) => {
             <form onSubmit={handleLogIn}>
                 <div className="formElement">
                     <label><h1>Username</h1></label>
-                    <input type="text" id="email" value={email} onChange={handleEmailChange} />
+                    <input type="text" id="email" placeholder={emailPlaceholder} 
+                        value={email} onChange={handleEmailChange} />
                 </div>
                 <div className="formElement">
                     <label><h1>Password</h1></label>
-                    <input type="password" id="password" value={password} onChange={handlePasswordChange}/>
+                    <input type="password" id="password" placeholder={passwordPlaceholder} 
+                        value={password} onChange={handlePasswordChange}/>
                 </div>
                 <div className="formElement">
                     <input type="submit" value="Next" />
