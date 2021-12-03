9e4880723e1c19e6e632486b941889b5db25dee7
zahir679 <zahirahmed0512@gmail.com>
2021-12-03 11:22:47 +0000

    bringing everything together

--- src/components/SiteComponents/PopupCard.js
+++ src/components/SiteComponents/PopupCard.js
@@ -13,29 +13,6 @@ const PopupCard = ({ restaurant, onClick, show }) => {
           
           <button className="popup-btn" onClick={onClick}>X</button>
           
-<<<<<<< HEAD
-          <div className="popup-info">
-          <h3>Name:</h3>
-          <h3>{restaurant.restaurantName}</h3>
-          <h4>Address:</h4>
-          <h4>{restaurant.address}</h4>
-          <h4>Cuisine:</h4>
-          <h4>{restaurant.cuisine}</h4>
-          <h4>Price:</h4>
-          <h4>{<Rating icon={ <CurrencyPoundIcon style={{fill: "green"}}/>} emptyIcon={<CurrencyPoundIcon/>} value={restaurant.price} precision={0.5}  readOnly />}</h4>
-          <h4>Vegetarian:</h4>
-          <h4>{restaurant.vegetarian ? "Yes" : "No"}</h4>
-          <h4>Halal:</h4>
-          <h4>{restaurant.halal ? "Yes" : "No"}</h4>
-          <h4>Gluten free:</h4>
-          <h4>{restaurant.glutenFree ? "Yes" : "No"}</h4>
-          <div className="reviewStuff">
-          <h4>Reviews:</h4>
-          <ReviewCardList reviews={restaurant.reviews} />
-          </div>
-
-
-=======
               <div className="popup-info">
                 <div className = "card-info">
                   <h3>Name:</h3>
@@ -71,7 +48,6 @@ const PopupCard = ({ restaurant, onClick, show }) => {
                   <b><a href="/LeaveAReview">Leave a Review!</a></b>
                 </div>
               </div>
->>>>>>> 010e735 (fixed css for pop up card)
 
 
           {/* <button type="button" onClick={handleClose}>
